72fbf79f033bda93e72003d289225506bff6755e
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2021-09-27 16:44:34 +0000

    Finished slide practice

---
+++ main.py
@@ -0,0 +1,5 @@
+# Heading
+
+# Name - Jeydin Pham
+# Date - September 27, 2021
+# Title - Slide Practice Unit 2 Functions
---
+++ slide_practice.py
@@ -0,0 +1,69 @@
+# Function Notation
+# f(x) = 5x + 2
+# x is considered our input, in the paranthesis f(x)
+# Outputvalue woud be 5x + 2 calculated
+
+# Functions (in Python): allow you to simplify
+# a problem and/or reuse the same code. The action of 
+# creating a function is known as "procedureal abstraction"
+# This all helps you organize your code
+
+# f(x) = 5x + 2 rewritten as a Python function
+def f(x): 
+	y = 5*x + 2
+	return y
+
+# Every function starts with "def"
+# Every function has a colon to end the first line
+# Most functions have parameters (input, our example, is x)
+# Every function has a name (here, ours is f)
+# All lines of code inside the function are indented
+
+# Using a function ("calling" a function)
+# Find the y value associated with f(x) when x = 10
+# f(10) = 5(10) + 2 = 52
+# If you want an answer from a function, store it
+
+y = f(10)
+# 10 is my parameter (input)
+# This will pass 10 through the function and return a value
+print(y)
+
+y1 = f(1) # 7
+y2 = f(2) # 12
+y3 = f(3) # 17
+
+# Functions must be defined BEFORE you call (use) them
+# Usually functions go at the top of your program
+# Examples of Functions
+
+def example_1(x):
+	ans = x ** x
+	return ans
+
+e2 = example_1(2) # = 4
+e5 = example_1(5) # = 3125
+print(e5 - e2) # = 3121
+
+# Example of a fuction that cubes
+def cube_it(x):
+	ans = x * x * x
+	return ans
+
+cube_4 = cube_it(4)
+print(cube_4) # = 64
+
+# Examples of a function that averages your grades
+# 60% for tests, 30% fpr quizzes, 10% for others
+def grade(test_avg, quiz_avg, other_avg):
+	# Three parameters (inputs)
+	total = (.60 * test_avg) + (.30 * quiz_avg) + (.10 * other_avg)
+	return total
+
+# Predict Mr P's six weeks grade in English
+x = 96
+y = 92
+z = 100
+# x, y, and z do not have to match the name of the parameters
+six_weeks_grade = int(grade(x, y, z))
+print("Mr. P's grade for the first six weeks is " + str(six_weeks_grade))
